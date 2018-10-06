03da41c4221038b4d46f52c9133ff088c78f9165
Wolfy-J <wolfy.jd@gmail.com>
2018-10-06 20:02:52 +0300

    init

---
+++ src/Config/TwigConfig.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Twig\Config;
+
+use Spiral\Core\Container\Autowire;
+use Spiral\Core\InjectableConfig;
+use Spiral\Twig\Exception\ConfigException;
+
+class TwigConfig extends InjectableConfig
+{
+    const CONFIG = "views/twig";
+
+    /** @var array */
+    protected $config = [
+        'options'    => [],
+        'extensions' => [],
+        'processors' => []
+    ];
+
+    public function getOptions(): array
+    {
+        return $this->config['options'];
+    }
+
+    /**
+     * @return Autowire[]
+     */
+    public function getExtensions(): array
+    {
+        $extensions = [];
+        foreach ($this->config['extensions'] as $extension) {
+            $extensions[] = $this->wire($extension);
+        }
+
+        return $extensions;
+    }
+
+    /**
+     * @return Autowire[]
+     */
+    public function getProcessors(): array
+    {
+        $processors = [];
+        foreach ($this->config['processors'] as $processor) {
+            $processors[] = $this->wire($processor);
+        }
+
+        return $processors;
+    }
+
+    /**
+     * @param mixed $item
+     * @return Autowire
+     *
+     * @throws ConfigException
+     */
+    public function wire($item): Autowire
+    {
+        if ($item instanceof Autowire) {
+            return $item;
+        }
+        if (is_string($item)) {
+            return new Autowire($item);
+        }
+        throw new ConfigException("Invalid class reference in view config.");
+    }
+}
---
+++ src/Exception/ConfigException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Twig\Exception;
+
+use Spiral\Core\Exception\RuntimeException;
+
+class ConfigException extends RuntimeException
+{
+
+}

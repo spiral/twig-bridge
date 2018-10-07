4d7acd8e9d9edee0bf7221f36dadfd4a7eee75ac
Wolfy-J <wolfy.jd@gmail.com>
2018-10-07 11:51:30 +0300

    more tests

--- tests/Twig/ConfigTest.php
+++ tests/Twig/ConfigTest.php
@@ -8,10 +8,11 @@
 
 namespace Spiral\Twig\Tests\Twig;
 
-use PHPUnit\Framework\TestCase;
+use Spiral\Core\Container\Autowire;
 use Spiral\Twig\Config\TwigConfig;
+use Spiral\Views\Processor\ContextProcessor;
 
-class ConfigTest extends TestCase
+class ConfigTest extends BaseTest
 {
     public function testOptions()
     {
@@ -20,5 +21,43 @@ class ConfigTest extends TestCase
         ]);
 
         $this->assertSame(['option' => 'value'], $config->getOptions());
+    }
+
+    public function testWireConfigString()
+    {
+        $config = new TwigConfig([
+            'processors' => [ContextProcessor::class]
+        ]);
+
+        $this->assertInstanceOf(
+            ContextProcessor::class,
+            $config->getProcessors()[0]->resolve($this->container)
+        );
+    }
+
+    public function testWireConfig()
+    {
+        $config = new TwigConfig([
+            'processors' => [
+                new Autowire(ContextProcessor::class)
+            ]
+        ]);
+
+        $this->assertInstanceOf(
+            ContextProcessor::class,
+            $config->getProcessors()[0]->resolve($this->container)
+        );
+    }
+
+    /**
+     * @expectedException \Spiral\Twig\Exception\ConfigException
+     */
+    public function testWireConfigException()
+    {
+        $config = new TwigConfig([
+            'processors' => [$this]
+        ]);
+
+        $config->getProcessors();
     }
 }
